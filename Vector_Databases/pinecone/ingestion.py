9a90bbd97382437bc5e539c050500953cb4e3789
Anjaiah Methuku <anjijava16@users.noreply.github.com>
2024-12-24 12:53:14 -0500

    Create ingestion.py

---
+++ Vector_Databases/pinecone/ingestion.py
@@ -0,0 +1,38 @@
+
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import ReadTheDocsLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+import os
+
+
+
+os.environ['OPENAI_API_KEY'] = ""
+os.environ['PINECONE_API_KEY'] = "e036-4b10-809c-ad52a7628531"
+os.environ["PINECONE_ENVIRONMENT"]="myapp"
+INDEX_NAME="lsample-movies"
+
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+
+
+def ingest_docs():
+    loader = ReadTheDocsLoader("langchain-docs/api.python.langchain.com/en/latest")
+
+    raw_documents = loader.load()
+    print(f"loaded {len(raw_documents)} documents")
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=600, chunk_overlap=50)
+    documents = text_splitter.split_documents(raw_documents)
+    for doc in documents:
+        new_url = doc.metadata["source"]
+        new_url = new_url.replace("langchain-docs", "https:/")
+        doc.metadata.update({"source": new_url})
+
+    print(f"Going to add {len(documents)} to Pinecone")
+    PineconeVectorStore.from_documents(documents, embeddings, index_name=INDEX_NAME)
+    print("****Loading to vectorstore done ***")
+
+
+if __name__ == "__main__":
+    ingest_docs()
